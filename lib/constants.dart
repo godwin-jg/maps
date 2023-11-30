396b970480529fa5d1684000e2f93b3d4bb61478
jg <jgnsecrets@gmail.com>
2023-11-30 23:59:13 +0530

    fITHh commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyBumOL17qUZfsuph0ohCl0RMaBEqhRC8Iw";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
