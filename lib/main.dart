816e9e68aed8de6528d1f757c3f8453465f7c6f0
jg <jgnsecrets@gmail.com>
2023-11-30 23:55:05 +0530

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,97 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:maps/path.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(OrderTrackingPage());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tourist Places',
-      home: TouristPlacesScreen(),
+      title: 'Google Places API Demo',
+      home: PlacesScreen(),
     );
   }
 }
 
-class TouristPlacesScreen extends StatelessWidget {
-  final List<String> touristPlaces = [
-    'Eiffel Tower, Paris',
-    'Machu Picchu, Peru',
-    'Great Wall of China, China',
-    'Pyramids of Giza, Egypt',
-    'Statue of Liberty, USA',
-    'Taj Mahal, India',
-    'Sydney Opera House, Australia',
-    'Colosseum, Italy',
-    'Santorini, Greece',
-    'Banff National Park, Canada',
-  ];
+class PlacesScreen extends StatefulWidget {
+  @override
+  _PlacesScreenState createState() => _PlacesScreenState();
+}
+
+String apiKey = 'AIzaSyBumOL17qUZfsuph0ohCl0RMaBEqhRC8Iw';
+String source = 'salem';
+String destination = 'chennai';
+
+class _PlacesScreenState extends State<PlacesScreen> {
+  final String apiUrl =
+      'https://maps.googleapis.com/maps/api/place/textsearch/json?query=$source+point+of+interest&language=en&radius=50000&key=$apiKey';
+
+  final String apiUrl2 =
+      'https://maps.googleapis.com/maps/api/place/textsearch/json?query=$destination+point+of+interest&language=en&radius=50000&key=$apiKey';
+  // https://maps.googleapis.com/maps/api/place/textsearch/json?query=dubai+point+of+interest&language=en&radius=2000&key
+
+  List<String> places = [];
+  List<String> places2 = [];
+  List<String> intersection = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      // print(response.body + " hello");
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        final List<dynamic> results = data['results'];
+        print(data);
+
+        setState(() {
+          places = results.map((place) => place['name'].toString()).toList();
+        });
+      }
+      final response2 = await http.get(Uri.parse(apiUrl2));
+      if (response2.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        final List<dynamic> results = data['results'];
+        print(data);
+
+        setState(() {
+          places2 = results.map((place) => place['name'].toString()).toList();
+        });
+        findIntersection();
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  void findIntersection() {
+    setState(() {
+      intersection = places.toSet().intersection(places2.toSet()).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tourist Places'),
+        title: Text('Places of Interest in New York City'),
       ),
       body: ListView.builder(
-        itemCount: touristPlaces.length,
+        itemCount: intersection.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(touristPlaces[index]),
-            onTap: () {
-              // You can add navigation or other actions when a place is tapped.
-              // For simplicity, we'll just print the place name for now.
-              print('Selected: ${touristPlaces[index]}');
-            },
+            title: Text(intersection[index]),
           );
         },
       ),
